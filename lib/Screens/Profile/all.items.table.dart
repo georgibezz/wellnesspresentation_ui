a562453ccb2b74af498120a4a7fbf28fe23a9480
BZDGEO001 <BZDGEO001@myuct.ac.za>
2023-08-13 18:03:40 +0200

    Adding files in project 'wellnesspresentation' to Git...

--- lib/Screens/Profile/all.items.table.dart
+++ lib/Screens/Profile/all.items.table.dart
@@ -26,6 +26,64 @@ class _AllItemsTableScreenState extends State<AllItemsTableScreen> {
     });
   }
 
+  Future<void> _showAddItemDialog() async {
+    final TextEditingController nameController = TextEditingController();
+    final TextEditingController descriptionController = TextEditingController();
+    final TextEditingController usageController = TextEditingController();
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add New Item'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(labelText: 'Name'),
+                ),
+                TextField(
+                  controller: descriptionController,
+                  decoration: InputDecoration(labelText: 'Description'),
+                ),
+                TextField(
+                  controller: usageController,
+                  decoration: InputDecoration(labelText: 'Usage'),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Add'),
+              onPressed: () async {
+                final newItem = Product(
+                  id: 0, // Replace with a unique ID
+                  name: nameController.text,
+                  description: descriptionController.text,
+                  usages: [usageController.text],
+                );
+
+                final itemBox = ObjectBoxService.objectBoxStore.box<Product>();
+                itemBox.put(newItem);
+
+                Navigator.of(context).pop();
+                await _loadItems();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,6 +107,12 @@ class _AllItemsTableScreenState extends State<AllItemsTableScreen> {
             ],
           );
         }).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await _showAddItemDialog();
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
